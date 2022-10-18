074d82d3983d19f80a05c1756364876c83caa698
Ra <ravicity999@gmail.com>
2022-10-19 05:10:05 +0530

    Fixed review extractor bugs in flipkart crawler

--- flipkart/working_review_exctractor.py
+++ flipkart/working_review_exctractor.py
@@ -20,22 +20,27 @@ newdata=list(orgdata)
 
 def extract_reviews(review_url):
     resp = rq.get(review_url)
+    print(type(resp))
     soup = bs(resp.text, 'html.parser')
-    reviews = soup.findAll('div', {'class':"_1AtVbE col-12-12"})
+    reviews = soup.findAll('div', {'class':"col _2wzgFH K0kLPL"})
 
     for single_review in reviews:
-        #print(single_review)
+        #print(len(reviews))
+
+        str_single_review = str(single_review)
+        dict_review = {
+                'productTitle': (str(review_url).split(".com/")[1]).split("product-reviews")[0],
+                'Review Title': str_single_review.split('class="_2-N8zT">')[1].split("</p>")[0],
+                #'Review Title': soup.find('p', {'class':"_2-N8zT"}).text.strip(),
+                #'Rating': (soup.find('div', {'class': '_3LWZlK _1BLPMq'}).get_text()),
+                'Rating': str_single_review.split('class="_3LWZlK _1BLPMq">')[1].split("<img class")[0],
+                #'Review Body': soup.find('div', {'class': 't-ZTKy'}).find().text.strip() ,
+                'Review Body': str_single_review.split('class="">')[1].split("</div>")[0]
+            }
         with open('outputs/file.html', 'w', encoding='utf-8') as f:
             f.write(str(single_review))
-    
-    dict_review = {
-            'productTitle': (str(review_url).split(".com/")[1]).split("product-reviews")[0],
-            'Review Title': soup.find('p', {'class':"_2-N8zT"}).text.strip(),
-            'Rating': (soup.find('div', {'class': '_3LWZlK _1BLPMq'}).get_text()),
-            'Review Body': soup.find('div', {'class': 't-ZTKy'}).find().text.strip() ,
-        }
-    
-    reviewlist.append(dict_review)
+        print(dict_review)
+        reviewlist.append(dict_review)
 
 def totalreviews(product_url):
     resp = rq.get(product_url)
@@ -44,7 +49,7 @@ def totalreviews(product_url):
     temp = reviews.text.split('\xa0')
 
     temp_list = temp[-1].split(" ")
-    print(temp_list[0])
+    #print(temp_list[0])
     return int(temp_list[0])
 
     #needs to return number of reviews count
@@ -66,25 +71,26 @@ def main():
         # product_url=newdata[url]
         review_url = product_url.replace("/p/", "/product-reviews/") + "&page=" + str(1)
         page_count=totalpages(review_url)
-        print(page_count)
+        #print(page_count)
         try:
-            print(review_url)
+            #print(review_url)
             review_count = totalreviews(review_url)
-            print("totalreviews",review_count)
+            #print("totalreviews",review_count)
         except:
             review_count = 0
 
-    for page_id in range(1,page_count):
-        print(f'Running for page{page_id}')
+    for page_id in range(1,page_count+1):
+        #print(f'Running for page{page_id}')
         try:
             review_url=product_url.replace("/p/", "/product-reviews/") + "&page=" + str(page_id)
-            print(review_url)
+            #print(review_url)
             extract_reviews(review_url)
         except Exception as e:
-            print("hi",e)
+            #print("hi",e)
             pass
 
     df=pd.DataFrame(reviewlist)
+    print(df)
     df.to_csv('flipkart_reviews.csv',mode='a',index=False)
 
 # product_url="https://www.flipkart.com/ryme-combo-ying-yang-dream-catcher-five-rings-multi-attract-positive-dreams-decorative-showpiece-55-cm/p/itm160b7e33e0fd4?pid=SHIF96HSGQEKNDX8&fm=organic&ppt=dynamic&ppn=dynamic&ssid=t989af0e740000001666083668826"
