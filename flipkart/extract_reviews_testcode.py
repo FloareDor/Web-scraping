57ce639f1e345dbe0e00a8c62ec9a8c9fe1c63c8
kvchaitanya <chaitanya4599@gmail.com>
2022-10-19 01:07:20 +0530

    updated all classes of flipkart

--- flipkart/extract_reviews_testcode.py
+++ flipkart/extract_reviews_testcode.py
@@ -24,21 +24,18 @@ def extract_reviews(review_url):
     reviews = soup.findAll('div', {'class':"_1AtVbE col-12-12"})
 
     for single_review in reviews:
-        print(single_review)
+        #print(single_review)
         with open('outputs/file.html', 'w', encoding='utf-8') as f:
             f.write(str(single_review))
     
-    # print(single_review.find('p', {'class':"_2-N8zT"}).text.strip())
-    # print(single_review.find('div', {'class': '_3LWZlK _1BLPMq'}).text.strip())
-    # print(single_review.find('div', {'class': 't-ZTKy'}).find().text.strip())
-    # dict_review = {
-    #         'productTitle': (str(review_url).split(".com/")[1]).split("product-reviews")[0],
-    #         'Review Title': single_review.find('p', {'class':"_2-N8zT"}).text.strip(),
-    #         'Rating': single_review.find('div', {'class': '_3LWZlK _1BLPMq'}).text.strip(),
-    #         'Review Body': single_review.find('div', {'class': 't-ZTKy'}).find().text.strip() ,
-    #     }
+    dict_review = {
+            'productTitle': (str(review_url).split(".com/")[1]).split("product-reviews")[0],
+            'Review Title': soup.find('p', {'class':"_2-N8zT"}).text.strip(),
+            'Rating': (soup.find('div', {'class': '_3LWZlK _1BLPMq'}).get_text()),
+            'Review Body': single_review.find('div', {'class': 't-ZTKy'}).find().text.strip() ,
+        }
     
-    # reviewlist.append(dict_review)
+    reviewlist.append(dict_review)
 
 def totalreviews(product_url):
     resp = rq.get(product_url)
@@ -77,20 +74,21 @@ def main():
         except:
             review_count = 0
 
-    for page_id in range(1,page_count):
+    for page_id in range(1,2):
         print(f'Running for page{page_id}')
         try:
             review_url=product_url.replace("/p/", "/product-reviews/") + "&page=" + str(page_id)
             print(review_url)
             extract_reviews(review_url)
         except Exception as e:
-            print(e)
+            print("hi",e)
             pass
 
     df=pd.DataFrame(reviewlist)
     df.to_csv('flipkart_reviews.csv',mode='a',index=False)
 
 # product_url="https://www.flipkart.com/ryme-combo-ying-yang-dream-catcher-five-rings-multi-attract-positive-dreams-decorative-showpiece-55-cm/p/itm160b7e33e0fd4?pid=SHIF96HSGQEKNDX8&fm=organic&ppt=dynamic&ppn=dynamic&ssid=t989af0e740000001666083668826"
-review_url="https://www.flipkart.com/ryme-combo-ying-yang-dream-catcher-five-rings-multi-attract-positive-dreams-decorative-showpiece-55-cm/product-reviews/itm160b7e33e0fd4?pid=SHIF96HSGQEKNDX8&lid=LSTSHIF96HSGQEKNDX8EZZTDR&marketplace=FLIPKART"
+# review_url="https://www.flipkart.com/ryme-combo-ying-yang-dream-catcher-five-rings-multi-attract-positive-dreams-decorative-showpiece-55-cm/product-reviews/itm160b7e33e0fd4?pid=SHIF96HSGQEKNDX8&lid=LSTSHIF96HSGQEKNDX8EZZTDR&marketplace=FLIPKART"
 # totalpages(review_url)
-extract_reviews(review_url)
+# extract_reviews(review_url)
+main()
