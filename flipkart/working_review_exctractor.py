6d6fa680baeb2eb10a36473b4b54f28c28cf8af8
Ra <ravicity999@gmail.com>
2022-10-19 06:18:10 +0530

    Fixed bugs for products with only 1 review page and other major bugs

--- flipkart/working_review_exctractor.py
+++ flipkart/working_review_exctractor.py
@@ -21,26 +21,36 @@ newdata=list(orgdata)
 def extract_reviews(review_url):
     resp = rq.get(review_url)
     print(type(resp))
-    soup = bs(resp.text, 'html.parser')
-    reviews = soup.findAll('div', {'class':"col _2wzgFH K0kLPL"})
-
+    try:
+        soup = bs(resp.text, 'html.parser')
+        reviews = soup.findAll('div', {'class':"col _2wzgFH K0kLPL"})
+        #review_count = soup.find('div', {'class':"_1AtVbE col-12-12"}).find('span').text.split("&")[1].split("Reviews")[0].strip()
+        #print(f"countttttttttt boiiiiii: {review_count}")
+        print(len(reviews))
+        #review_count = int(review_count)
+    except Exception as e:
+        print(e)
     for single_review in reviews:
         #print(len(reviews))
-
+        #single_review = reviews[i]
         str_single_review = str(single_review)
-        dict_review = {
-                'productTitle': (str(review_url).split(".com/")[1]).split("product-reviews")[0],
-                'Review Title': str_single_review.split('class="_2-N8zT">')[1].split("</p>")[0],
-                #'Review Title': soup.find('p', {'class':"_2-N8zT"}).text.strip(),
-                #'Rating': (soup.find('div', {'class': '_3LWZlK _1BLPMq'}).get_text()),
-                'Rating': str_single_review.split('class="_3LWZlK _1BLPMq">')[1].split("<img class")[0],
-                #'Review Body': soup.find('div', {'class': 't-ZTKy'}).find().text.strip() ,
-                'Review Body': str_single_review.split('class="">')[1].split("</div>")[0]
-            }
+        try:
+            dict_review = {
+                    'productTitle': (str(review_url).split(".com/")[1]).split("product-reviews")[0],
+                    'Review Title': str_single_review.split('class="_2-N8zT">')[1].split("</p>")[0],
+                    #'Review Title': soup.find('p', {'class':"_2-N8zT"}).text.strip(),
+                    #'Rating': (soup.find('div', {'class': '_3LWZlK _1BLPMq'}).get_text()),
+                    'Rating': str_single_review.split('class="_3LWZlK _1BLPMq">')[1].split("<img class")[0],
+                    #'Review Body': soup.find('div', {'class': 't-ZTKy'}).find().text.strip() ,
+                    'Review Body': str_single_review.split('class="">')[1].split("</div>")[0].replace("READ MORE", "")
+                }
+        except Exception as e:
+            print(f"exceeeeeeeeeeeeeeppppppppptioooooooon{e}")
         with open('outputs/file.html', 'w', encoding='utf-8') as f:
             f.write(str(single_review))
         print(dict_review)
         reviewlist.append(dict_review)
+
 
 def totalreviews(product_url):
     resp = rq.get(product_url)
@@ -57,8 +67,11 @@ def totalreviews(product_url):
 def totalpages(review_url):
     resp = rq.get(review_url)
     soup = bs(resp.text, 'html.parser')
-    pages = soup.find('div', {'class':"_2MImiq _1Qnn1K"}).find('span')
-    temp = pages.text.split(" ")
+    try:
+        pages = soup.find('div', {'class':"_2MImiq _1Qnn1K"}).find('span')
+        temp = pages.text.split(" ")
+    except:
+        return 1
     # return pages.text
     return(int(temp[-1]))
     #needs to return number of pages
@@ -67,9 +80,10 @@ def main():
     product_url="https://www.flipkart.com/ryme-combo-ying-yang-dream-catcher-five-rings-multi-attract-positive-dreams-decorative-showpiece-55-cm/p/itm160b7e33e0fd4?pid=SHIF96HSGQEKNDX8&fm=organic&ppt=dynamic&ppn=dynamic&ssid=t989af0e740000001666083668826"
     # totalreviews(product_url)
     revlist=[] #empty revlist
-    for url in range(1): #ideally should be newdata
-        # product_url=newdata[url]
+    for url in range(1,3): #ideally should be newdata
+        product_url=newdata[url]
         review_url = product_url.replace("/p/", "/product-reviews/") + "&page=" + str(1)
+        print(review_url)
         page_count=totalpages(review_url)
         #print(page_count)
         try:
@@ -79,15 +93,15 @@ def main():
         except:
             review_count = 0
 
-    for page_id in range(1,page_count+1):
-        #print(f'Running for page{page_id}')
-        try:
-            review_url=product_url.replace("/p/", "/product-reviews/") + "&page=" + str(page_id)
-            #print(review_url)
-            extract_reviews(review_url)
-        except Exception as e:
-            #print("hi",e)
-            pass
+        for page_id in range(1,page_count+1):
+            #print(f'Running for page{page_id}')
+            try:
+                review_url=product_url.replace("/p/", "/product-reviews/") + "&page=" + str(page_id)
+                #print(review_url)
+                extract_reviews(review_url)
+            except Exception as e:
+                print("hi",e)
+                pass
 
     df=pd.DataFrame(reviewlist)
     print(df)
